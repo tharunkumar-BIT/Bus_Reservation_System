f79d0bef6953f9598017670530a3d5e5802b31b1
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-05-24 21:36:12 +0530

    bus class

--- src/busResv/BusDemo.java
+++ src/busResv/BusDemo.java
@@ -1,11 +1,23 @@
 package busResv;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class BusDemo {
 	public static void main(String[] args) {
 		
+		ArrayList<Bus> buses = new ArrayList<Bus>();
+		
+		buses.add(new Bus(1,true, 45));
+		buses.add(new Bus(2, false, 50));
+		buses.add(new Bus(3, true, 48));
+		
 		int userOpt = 1;
 		Scanner scanner = new Scanner(System.in);
+		
+		for(Bus b : buses) {
+			b.displayBusInfo();
+		}
+		
 		while(userOpt == 1) {
 			System.out.println("Enter 1 to Book amd 2 to exit");
 			userOpt = scanner.nextInt();
